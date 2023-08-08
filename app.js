ecec8f3e46b6e2d247730193bc301e25041814a8
Sunjay Varshan <111437799+Sunjayvarshan@users.noreply.github.com>
2023-08-08 21:50:05 +0530

    setting up config files

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const app = require('./app');
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
 });

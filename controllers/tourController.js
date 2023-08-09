7eae9c2e76cd8ef56f4db7e59a239cc21b103b61
Sunjay Varshan <111437799+Sunjayvarshan@users.noreply.github.com>
2023-08-09 21:04:11 +0530

    connected to a mongo db

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
 exports.checkID = (req, res, next, val) => {
@@ -62,7 +62,7 @@ exports.createTour = (req, res) => {
           tour: newTour,
         },
       });
-    }
+    },
   );
 };
 

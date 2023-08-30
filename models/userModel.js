ba262906b077227f0de2ef807ae4eb3eed07e50b
Sunjay Varshan <111437799+Sunjayvarshan@users.noreply.github.com>
2023-08-30 19:49:10 +0530

    notorus password bcrypt

---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      //only works on save
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'the passwords dont match',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  //only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

767066d283d6a6f18dd676dece8d4c75d531a8ea
Sunjay Varshan <111437799+Sunjayvarshan@users.noreply.github.com>
2023-09-09 13:58:58 +0530

    auth bits complete

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
@@ -30,6 +33,26 @@ app.use('/api', limiter);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//data sanitization against XSS
+app.use(xss());
+
+//prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));

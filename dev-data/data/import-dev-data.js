1731e7c13b319dd75e87216c8182d54c6eac77a4
Sunjay Varshan <111437799+Sunjayvarshan@users.noreply.github.com>
2023-09-11 17:17:20 +0530

    typo

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -18,9 +18,7 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 const importData = async () => {
   try {
--- models/tourModel.js
+++ models/tourModel.js
@@ -78,6 +78,29 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    location: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
